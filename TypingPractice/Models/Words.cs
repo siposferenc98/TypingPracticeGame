657c01d92dcb639663bbf484849fccad69ee7558
Ferenc Sipos <ferenc.m.sipos@gmail.com>
2022-02-08 22:01:24 +0100

    endofline words?

--- TypingPractice/Models/Words.cs
+++ TypingPractice/Models/Words.cs
@@ -5,16 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using TypingPractice.Properties;
+using System.IO;
 
 namespace TypingPractice.Models
 {
     internal class Words
     {
-        private List<string> WordsList = Resources.ResourceManager.GetString("Words")!.Split().ToList();
+        private List<string> WordsList = Resources.ResourceManager.GetString("Words")!.Split(Environment.NewLine).ToList();
 
         public Words()
         {
-
+            
         }
 
         public string GetRandomWord()
